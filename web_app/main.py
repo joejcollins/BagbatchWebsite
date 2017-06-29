4a0ea2a88802ec2f6cab81e1e7fcad3029770e48
Joe J Collins <joe_collins@blackradley.com>
2017-06-29 15:22:04 +0000

    Works on Cloud 9

--- web_app/main.py
+++ web_app/main.py
@@ -1,15 +1,16 @@
+import os
 import logging
 
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Hello World of Flask!'
 
+app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
 
 @app.errorhandler(500)
 def server_error(e):
