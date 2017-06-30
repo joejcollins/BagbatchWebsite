cba35da7bda96a2682cdf7885d0d59f0957c19b2
Joe Colllins <joejcollins@gmail.com>
2017-06-30 14:29:59 +0100

    Import Flask like this....

--- web_app/main.py
+++ web_app/main.py
@@ -1,16 +1,30 @@
-import os
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START app]
 import logging
 
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello World of Flask!'
+    return 'Hello World!'
 
-app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
 
 @app.errorhandler(500)
 def server_error(e):
