46fdc026809b63f71e0fdc6bd44632be79709d18
Joe J Collins <joejcollins@gmail.com>
2017-07-03 10:00:58 +0100

    Start mail message

--- web_app/appengine_config.py
+++ web_app/appengine_config.py
@@ -1,25 +1,14 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-import logging, os, sys
+''' App Engine Configuration '''
+import logging
+import os
+import sys
 from google.appengine.ext import vendor
 
-
-'''logging.info("sys platform: " + sys.platform)
+logging.info("sys platform: " + sys.platform)
 logging.info("os name: " + os.name)
-logging.info("server software: " + os.environ.get('SERVER_SOFTWARE',''))'''
+logging.info("server software: " + os.environ.get('SERVER_SOFTWARE', ''))
 
-on_appengine = os.environ.get('SERVER_SOFTWARE', '').startswith('Development')
+on_appengine = os.environ.get('SERVER_SOFTWARE', '').startswith('Development')# pylint: disable=invalid-name
 if on_appengine and os.name == 'nt':
     sys.platform = "Not Windows"
 
--- web_app/main.py
+++ web_app/main.py
@@ -1,54 +1,48 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+''' main application '''
 
+import json
 import logging
 from flask import Flask, render_template, request
+from google.appengine.api import mail
 
-# [START create_app]
 app = Flask(__name__) # pylint: disable=invalid-name
-# [END create_app]
 
 @app.route('/')
 def hello():
+    ''' Just a test message '''
     return 'Hello World!'
 
-# [START form]
 @app.route('/form')
 def form():
+    ''' Show the message form '''
     return render_template('form.html')
-# [END form]
 
-
-# [START submitted]
 @app.route('/submitted', methods=['POST'])
 def submitted_form():
+    ''' Respond to the message submission '''
     email = request.form['email']
     message = request.form['message']
-
-    # [END submitted]
-    # [START render_template]
+    send_mail() # Send
     return render_template(
         'submitted_form.html',
         email=email,
         message=message)
-    # [END render_template]
 
+def send_mail():
+    ''' Send an email message '''
+    secrets_file = open('secrets.json', 'r')
+    secrets = json.loads(secrets_file.read())
+    secrets_file.close()
+    message = mail.EmailMessage(
+        sender="a@b.com",
+        subject="Your account has been approved")
+    message.to = secrets['email'],
+    message.body = "Dear Albert"
+    message.send()
 
-@app.errorhandler(500)
-def server_error(error):
-    ''' Log any errors and send 500 '''
-    # Log the error and stacktrace.
-    logging.exception('An error occurred during a request. ' + error.msg)
-    return 'An internal error occurred.', 500
-# [END app]
+# @app.errorhandler(500)
+# def server_error(error):
+#     ''' Log any errors and send 500 '''
+#     # Log the error and stacktrace.
+#     logging.exception('An error occurred during a request. ' + error)
+#     return 'An internal error occurred.', 500
