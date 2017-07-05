4d1539b09a652f6d6c6148a881dc832918c1efa7
Joe Colllins <joejcollins@gmail.com>
2017-07-05 15:36:34 +0100

    CSRF is automatic and error messages.

--- web_app/main.py
+++ web_app/main.py
@@ -2,16 +2,13 @@
 import logging
 import secrets
 import forms
-from flask import Flask, flash, render_template, request
-from flask_wtf.csrf import CSRFProtect
+from flask import Flask, render_template
 from google.appengine.api import app_identity # pylint: disable=E0401
 from google.appengine.api import mail # pylint: disable=E0401
 
 # Initialize the application with CSRF
 app = Flask(__name__) # pylint: disable=invalid-name
 app.secret_key = secrets.SECRET_KEY
-CSRF = CSRFProtect(app)
-CSRF.init_app(app)
 app.config['RECAPTCHA_USE_SSL'] = False
 app.config['RECAPTCHA_PUBLIC_KEY'] = secrets.RECAPTCHA_PUBLIC_KEY
 app.config['RECAPTCHA_PRIVATE_KEY'] = secrets.RECAPTCHA_PRIVATE_KEY
@@ -22,7 +19,8 @@ def form():
     ''' Show the message form '''
     message_form = forms.MessageForm()
     if message_form.validate_on_submit():
-        return render_template('submitted_form.html', form=message_form)
+        send_mail(message_form.email.data, message_form.message.data)
+        return render_template('submitted_form.html', title="Thanks", form=message_form)
     return render_template('form.html', title="Message", form=message_form)
 
 def send_mail(their_email, their_message):
