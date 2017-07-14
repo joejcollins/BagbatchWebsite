81ee394715429d72936d0d2bdbd413ea8fe41c8d
Joe Colllins <joejcollins@gmail.com>
2017-07-14 12:20:42 +0100

    Show error messages in the browser.

--- web_app/main.py
+++ web_app/main.py
@@ -1,18 +1,27 @@
-''' SController for the application '''
+''' Controller for the application '''
 import logging
 import secrets
 import forms
+import sys, traceback, os
 from flask import Flask, render_template
 from google.appengine.api import app_identity # pylint: disable=E0401
 from google.appengine.api import mail # pylint: disable=E0401
 
 # Initialize the application with CSRF
 app = Flask(__name__) # pylint: disable=invalid-name
+# Set the Flask debug to false so you can use GAE debug
+app.config.update(DEBUG = False)
 app.secret_key = secrets.SECRET_KEY
 app.config['RECAPTCHA_USE_SSL'] = False
 app.config['RECAPTCHA_PUBLIC_KEY'] = secrets.RECAPTCHA_PUBLIC_KEY
 app.config['RECAPTCHA_PRIVATE_KEY'] = secrets.RECAPTCHA_PRIVATE_KEY
 app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
+
+@app.before_request
+def enable_local_error_handling():
+    ''' test of log  '''
+    app.logger.addHandler(logging.StreamHandler())
+    app.logger.setLevel(logging.INFO)
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
@@ -34,7 +43,15 @@ def send_mail(their_email, their_message):
 
 @app.errorhandler(500)
 def server_error(error):
-    ''' Log any errors and send 500 '''
-    # Log the error and stacktrace.
+    ''' Log any errors to the browser because you are too lazy to look at the console
+    
+    The Flask DEBUG setting must the set to false for this to work '''
+    recent_exception = sys.exc_info()
+    exception_type = recent_exception[0]
+    exception_value = recent_exception[1]
+    trace_back = recent_exception[2]
+    no_limit = None
+    exception = ''.join(traceback.format_exception(exception_type, exception_value,
+                                                   trace_back, no_limit))
     logging.exception('An error occurred during a request. ' + str(error))
-    return 'An internal error occurred.', 500
+    return render_template('500.html', title=error, exception=exception)
